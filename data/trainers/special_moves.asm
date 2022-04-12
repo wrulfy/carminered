b03809d73c03d868048325aa2784085cc526e71d
wrulfy <omegajynx@hotmail.es>
2022-04-12 23:00:33 +0200

    Ported Pokeyellow's read trainer party
    I can't believe it just works

--- data/trainers/special_moves.asm
+++ data/trainers/special_moves.asm
@@ -1,25 +1,12 @@
-; unique moves for gym leaders
-; this is not automatic! you have to write the index you want to [wLoneAttackNo]
-; first. e.g., erika's script writes 4 to [wLoneAttackNo] to get mega drain,
-; the fourth entry in the list.
-LoneMoves:
-	; pokemon index, move to give nth pokemon
-	db 2, SWIFT
-	db 2, MIST
-	db 3, SMOKESCREEN
-	db 3, CONSTRICT
-	db 4, POISON_GAS
-	db 4, CONFUSE_RAY
-	db 5, THUNDER_WAVE
-	db 5, EARTHQUAKE
+; Yellow entry format:
+;	db trainerclass, trainerid
+;	repeat { db partymon location, partymon move, move id }
+;	db 0
 
-; unique moves for elite 4
-; all trainers in this class are given this move automatically
-; (unrelated to LoneMoves)
-TeamMoves:
-	; trainer, move
-	db LORELEI, HYDRO_PUMP
-	db BRUNO,   FLAMETHROWER
-	db AGATHA,  EGG_BOMB
-	db LANCE,   THUNDERBOLT
+SpecialTrainerMoves:
+	db RIVAL1, 1
+	db 1, 1, PECK
+	db 1, 2, SEISMIC_TOSS
+	db 0
+
 	db -1 ; end
