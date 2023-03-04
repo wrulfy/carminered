9536765620050eabbe086474318e01a99a497301
wrulfy <omegajynx@hotmail.es>
2023-03-04 23:14:54 +0100

    seafoam dialogue
    I had this on the laptop only

--- text/SeafoamIslandsB4F.asm
+++ text/SeafoamIslandsB4F.asm
@@ -12,3 +12,35 @@ _SeafoamIslands5Text5::
 	text "DANGER"
 	line "Fast current!"
 	done
+
+_SeafoamIslands5BattleText1::
+	text "Did you got lost"
+	line "down here too?"
+	done
+
+_SeafoamIslands5EndBattleText1::
+	text "Maybe"
+	line "if we climb up..."
+	prompt
+
+_SeafoamIslands5AfterBattleText1::
+	text "My partner and I"
+	line "fell down a hole,"
+	cont "now we're stuck!"
+	done
+
+_SeafoamIslands5BattleText2::
+	text "Hey, could you"
+	line "help us out?"
+	done
+
+_SeafoamIslands5EndBattleText2::
+	text "These"
+	line "darned currents!"
+	prompt
+
+_SeafoamIslands5AfterBattleText2::
+	text "I had planned a"
+	line "a romantic trip,"
+	cont "not this!"
+	done
